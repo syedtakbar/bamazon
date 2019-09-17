d927e3df7286506c36f7c4af73a7088052c0baba
Syed T. Akbar <sakbar01@icloud.com>
2019-09-16 21:23:36 -0500

    added some validation

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,15 @@
 // This application is designed and developed by Syed Akbar (September 16, 2019)
 const config = require('./config');
 
-const libBamazonCust = {
-     inquirer : require("inquirer"), 
-     mysql : require("mysql"),     
-     fs : require("fs"),                   
-     colors : require('colors'),
-     pad : require('pad'),
-     productionQuestions : [],
-     genQuestions : (obj) => {                         
+const BamazonCust = {
+    inquirer : require("inquirer"), 
+    mysql : require("mysql"),     
+    fs : require("fs"),                   
+    colors : require('colors'),
+    pad : require('pad'),
+    productionQuestions : [],
+
+    genQuestions : (obj) => {                         
         return [
                             {
                                 type: "list",
@@ -19,7 +20,7 @@ const libBamazonCust = {
                             {
                                 type: "input",
                                 name: "quantity",
-                                message: "Please enter quantity for your selection: ",                        
+                                message: "Please enter quantity for your selection: ",                                                        
                                 filter: function(val) {
                                 return val.toLowerCase();
                                 }                       
@@ -27,24 +28,23 @@ const libBamazonCust = {
                         ];
      },
     
-    
     preprompt: () => {            
         let sql = `CALL getProducts()`;
-        let connection = libBamazonCust.mysql.createConnection(config);
+        let connection = BamazonCust.mysql.createConnection(config);
         
         connection.query(sql, [], (error, results, fields) => {
                     if (error) {
                         return console.error(error.message);
                     }
-                    libBamazonCust.productionQuestions = results[0].map(x => x.item_id + " - " + x.product_name + " $" + x.price + " #" + x.stock_quantity);                     
-                    libBamazonCust.prompt(libBamazonCust.productionQuestions );                
+                    BamazonCust.productionQuestions = results[0].map(x => x.item_id + " - " + x.product_name + " $" + x.price + " #" + x.stock_quantity);                     
+                    BamazonCust.prompt(BamazonCust.productionQuestions );                
                     connection.end();                
         });    
     },
 
     processCustomerOrder: (itemId, quantityOrdered, UpdateCallBack) => {            
         let sql = `CALL getProduct(?)`;
-        let connection = libBamazonCust.mysql.createConnection(config);
+        let connection = BamazonCust.mysql.createConnection(config);
         
         connection.query(sql, [itemId], (error, results) => {
                     if (error) {
@@ -64,7 +64,7 @@ const libBamazonCust = {
         
         if (parseInt(stockQuantity) < parseInt(quantityOrdered)) 
         {
-            console.log(libBamazonCust.colors.red(`
+            console.log(BamazonCust.colors.red(`
                                                     Insufficient quantity for ItemId: ${itemId}
                                                     Quantity Orderd: ${quantityOrdered}
                                                     Stocked Quantity: ${stockQuantity}                                    
@@ -74,7 +74,7 @@ const libBamazonCust = {
         
         
         let sql = `CALL updateProduct(?,?)`;
-        let connection = libBamazonCust.mysql.createConnection(config);
+        let connection = BamazonCust.mysql.createConnection(config);
         
         connection.query(sql, [itemId, quantityOrdered], (error, results) => {
                     if (error) {
@@ -82,7 +82,7 @@ const libBamazonCust = {
                     }
                     
                     const totalAmt = quantityOrdered * price;
-                    console.log(libBamazonCust.colors.green (
+                    console.log(BamazonCust.colors.green (
                                 ` 
                                     Order successful for Item ${itemId} and quantity ${quantityOrdered}
                                     Order Total: $${totalAmt}
@@ -92,26 +92,27 @@ const libBamazonCust = {
         });    
     },  
 
-    prompt : (obj) => libBamazonCust.inquirer.prompt(libBamazonCust.genQuestions(obj)).then(libBamazonCust.processAnswer,libBamazonCust.processError),
+    prompt : (obj) => BamazonCust.inquirer.prompt(BamazonCust.genQuestions(obj)).then(BamazonCust.processAnswer,BamazonCust.processError),
 
     processAnswer: (answers) => {      
         const item_id = answers.item.substring(0,10);
-        const quantity = answers.quantity || 0;
+        if (isNaN ( answers.quantity)) {console.log(BamazonCust.colors.red(`Please enter a valid quantity and try again!`)); return;}
 
-        if (quantity === 0) {console.log(libBamazonCust.colors.red(`Please enter a valid quantity and try again!`)); return;}
+        const quantity = answers.quantity || 0;
+        if (quantity === 0) {console.log(BamazonCust.colors.red(`Please enter a valid quantity and try again!`)); return;}
         //console.log(`item selected: ${item_id} and quantity: ${quantity}`);
-        libBamazonCust.processCustomerOrder(item_id, quantity, libBamazonCust.updateOrderQuantity);
+        BamazonCust.processCustomerOrder(item_id, quantity, BamazonCust.updateOrderQuantity);
     },
     
 
     processError: (err) => {                
-        if (err) console.log(libBamazonCust.colors.red(err));                                                         
+        if (err) console.log(BamazonCust.colors.red(err));                                                         
     },
 
     writeData: (content) => {        
-        libBamazonCust.fs.appendFile("tran.txt", content, libBamazonCust.processError );
+        BamazonCust.fs.appendFile("tran.txt", content, BamazonCust.processError );
     },
 };
 
 
-libBamazonCust.preprompt();
+BamazonCust.preprompt();
